c09110cae8b4b3aff32000ac7d50bc200e34b3b7
yahirGit <yabsolano@hotmail.com>
2024-05-22 12:07:14 -0500

    yahirGit: add 15_ suma de los digitos de un numero entero de 3 digitos

---
+++ ejercicios_decisiones/15_suma_digitos_entero.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main(){
+
+    printf("\nPrograma num 15. Leer un numero entero de tres digitos y determinar a cuanto.\n");
+    printf("\n============\n");
+
+    //Declaramos variables
+    //num1 = numero uno
+    //num2 = numero dos
+    int num, resultSuma, uDigito,dDigito,tDigito;
+
+    printf("Digite el numero uno: ");
+    //leer las variables y almacenar su valor
+    scanf("%d",&num);
+    
+    if (num < 0){
+        num = -num;
+    }
+
+    if ((num >= 100 && num <=999)){
+        uDigito = num / 100;
+        dDigito = (num % 100) / 10;
+        tDigito = num % 10;
+
+        resultSuma = uDigito + dDigito + tDigito;
+
+        //printf("Los digitos son uno:%d dos:%d tres:%d\n",uDigito,dDigito,tDigito);
+
+        printf("La suma de todos los digitos del numero: %d es la siguiente = %d\n",num,resultSuma);
+
+        
+    }else{
+         printf("Digite un numero de tres digitos");
+    }
+
+    return 0;
+}
