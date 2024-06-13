d374462f577eb9e908298b79d9ac2e6d3aa7c356
yahirGit <yabsolano@hotmail.com>
2024-06-12 20:30:25 -0500

    yahirGit: add 44_ el primer digito es multiplo de alguno de los otros digitos

--- ejercicios_decisiones/44_entero_4_d_multiplo.cpp
+++ ejercicios_decisiones/44_entero_4_d_multiplo.cpp
@@ -18,11 +18,28 @@ int main(){
     if (numero < 0){
         numero = -numero;
     }
-    
-    if(numero >= 1000 && numero <= 9999){
-        cout << "El numero es de 4 digitos" << endl;
+
+    digito1 = (numero / 1000) % 100;
+    digito2 = (numero / 100) % 10;
+    digito3 = (numero / 10 ) % 10;
+    digito4 = numero % 10;
+
+    if (numero >= 1000 && numero <= 9999){
+
+        //Como se tiene que comprobar que el primero digito al dividirlo por el segun sea igual a cero para saber si es multiplo de ese numero
+        //Se tiene que comprobar primero que los digitos no sean 0 porque una division por cero es un error.
+        if (digito2 != 0 && digito1 % digito2 == 0) {
+            cout << "El primer digito es multiplo del segundo digito." << endl;
+        }else if (digito3 != 0 && digito1 % digito3 == 0) {
+            cout << "El primer digito es multiplo del tercer digito." << endl;
+        }else if (digito4 != 0 && digito1 % digito4 == 0) {
+            cout << "El primer digito es multiplo del cuarto digito." << endl;
+        }else{
+            cout << "El primer digito no es multiplo de ninguno de los otros digitos." << endl;
+        }
+        
     }else{
-        cout << "El numero tiene que ser de 4 digitos." << endl;
+        cout << "Solo se permiten numeros de 4 digitos." << endl;
     }
 
     return 0;
